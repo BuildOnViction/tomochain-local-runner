52a646c8681f710b4d63f81971163de051331ac1
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-09-09 09:20:33 +0000

    fullnode run mongodb

--- tomox.sh
+++ tomox.sh
@@ -20,13 +20,16 @@ then
   wallet1=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account import --password .pwd --datadir ./nodes/1 <(echo ${PRIVATE_KEY_1}) | awk -v FS="({|})" '{print $2}')
   wallet2=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account import --password .pwd --datadir ./nodes/2 <(echo ${PRIVATE_KEY_2}) | awk -v FS="({|})" '{print $2}')
   wallet3=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account import --password .pwd --datadir ./nodes/3 <(echo ${PRIVATE_KEY_3}) | awk -v FS="({|})" '{print $2}')
+  wallet4=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account import --password .pwd --datadir ./nodes/4 <(echo ${PRIVATE_KEY_4}) | awk -v FS="({|})" '{print $2}')
   ${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo --datadir ./nodes/1 init ./genesis/genesis.json
   ${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo --datadir ./nodes/2 init ./genesis/genesis.json
   ${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo --datadir ./nodes/3 init ./genesis/genesis.json
+  ${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo --datadir ./nodes/4 init ./genesis/genesis.json
 else
   wallet1=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account list --datadir ./nodes/1 | head -n 1 | awk -v FS="({|})" '{print $2}')
   wallet2=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account list --datadir ./nodes/2 | head -n 1 | awk -v FS="({|})" '{print $2}')
   wallet3=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account list --datadir ./nodes/3 | head -n 1 | awk -v FS="({|})" '{print $2}')
+  wallet4=$(${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo account list --datadir ./nodes/4 | head -n 1 | awk -v FS="({|})" '{print $2}')
 fi
 
 VERBOSITY=3
@@ -54,7 +57,6 @@ ${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo \
     --announce-txs \
     --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8545 --rpcvhosts "*" \
     --rpcapi "personal,db,eth,net,web3,txpool,miner,tomoX" \
-    --gcmode "archive" \
     --ws --wsaddr 0.0.0.0 --wsport 8546 --wsorigins "*" --unlock "${wallet1}" \
 	--ethstats "sun:test2test@localhost:3004" \
     --password ./.pwd --mine --gasprice "${GASPRICE}" --targetgaslimit "420000000" --verbosity ${VERBOSITY} &
@@ -74,8 +76,18 @@ ${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo \
     --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@127.0.0.1:30301" \
     --syncmode "full" --datadir ./nodes/3 --networkid 89 --port 30305 \
     --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8548 --rpcvhosts "*" \
-    --tomox --tomox.datadir "$WORK_DIR/nodes/3/tomox" --tomox.dbengine "mongodb" \
-    --rpcapi "personal,db,eth,net,web3,txpool,miner,tomoX" \
+    --tomox --tomox.datadir "$WORK_DIR/nodes/3/tomox" --tomox.dbengine "leveldb" \
     --unlock "${wallet3}" --password ./.pwd --mine --gasprice "${GASPRICE}" \
 	--ethstats "earth:test2test@localhost:3004" \
+    --targetgaslimit "420000000" --verbosity ${VERBOSITY} &
+child_proc="$child_proc $!"
+
+${TOMOCHAIN_PROJECT_DIR}/build/bin/tomo \
+    --bootnodes "enode://7d8ffe6d28f738d8b7c32f11fb6daa6204abae990a842025b0a969aabdda702aca95a821746332c2e618a92736538761b1660aa9defb099bc46b16db28992bc9@127.0.0.1:30301" \
+    --syncmode "full" --datadir ./nodes/4 --networkid 89 --port 30306 \
+    --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcport 8549 --rpcvhosts "*" \
+    --rpcapi "personal,db,eth,net,web3,txpool,miner,tomoX" \
+    --tomox --tomox.datadir "$WORK_DIR/nodes/4/tomox" --tomox.dbengine "mongodb" \
+    --unlock "${wallet4}" --password ./.pwd --mine --gasprice "${GASPRICE}" \
+	--ethstats "tomox-fullnode:test2test@localhost:3004" \
     --targetgaslimit "420000000" --verbosity ${VERBOSITY}
